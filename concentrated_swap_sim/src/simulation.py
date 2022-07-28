86766e215a3dbe94262b3ea7b3a73d339123a3e1
Timofey <epanchee@gmail.com>
2022-07-28 16:53:54 +0500

    ported math functions from vyper

--- concentrated_swap_sim/src/simulation.py
+++ concentrated_swap_sim/src/simulation.py
@@ -22,11 +22,11 @@ def geometric_mean(x):
 
 def reduction_coefficient(x, gamma):
     N = len(x)
-    x_prod = 10 ** 18
+    # x_prod = 10 ** 18
     K = 10 ** 18
     S = sum(x)
     for x_i in x:
-        x_prod = x_prod * x_i // 10 ** 18
+        # x_prod = x_prod * x_i // 10 ** 18
         K = K * N * x_i // S
     if gamma > 0:
         K = gamma * 10 ** 18 // (gamma + 10 ** 18 - K)
@@ -69,6 +69,87 @@ def newton_D(A, gamma, x, D0):
             return D
 
     raise ValueError("Did not converge")
+
+
+N_COINS = 2
+
+
+def unsafe_div(a, b):
+    return a // b
+
+
+def unsafe_sub(a, b):
+    return a - b
+
+
+def newton_y_vyper(ANN, gamma, x, D, i):
+    """Ported from Vyper code"""
+
+    x_j = x[1 - i]
+    y = D ** 2 // (x_j * N_COINS ** 2)
+    K0_i = (10 ** 18 * N_COINS) * x_j // D
+    # S_i = x_j
+
+    # frac = x_j * 1e18 / D => frac = K0_i / N_COINS
+    assert (K0_i > 10 ** 16 * N_COINS - 1) and (K0_i < 10 ** 20 * N_COINS + 1)  # dev: unsafe values x[i]
+
+    # x_sorted[N_COINS] = x
+    # x_sorted[i] = 0
+    # x_sorted = self.sort(x_sorted)  # From high to low
+    # x[not i] instead of x_sorted since x_soted has only 1 element
+
+    convergence_limit = max(max(x_j // 10 ** 14, D // 10 ** 14), 100)
+
+    __g1k0 = gamma + 10 ** 18
+
+    for j in range(255):
+        y_prev = y
+
+        K0 = unsafe_div(K0_i * y * N_COINS, D)
+        S = x_j + y
+
+        _g1k0 = __g1k0
+        if _g1k0 > K0:
+            _g1k0 = unsafe_sub(_g1k0, K0) + 1
+        else:
+            _g1k0 = unsafe_sub(K0, _g1k0) + 1
+
+        # D / (A * N**N) * _g1k0**2 / gamma**2
+        mul1 = unsafe_div(unsafe_div(unsafe_div(10 ** 18 * D, gamma) * _g1k0, gamma) * _g1k0 * A_MULTIPLIER, ANN)
+
+        # 2*K0 / _g1k0
+        mul2 = unsafe_div(10 ** 18 + (2 * 10 ** 18) * K0, _g1k0)
+
+        yfprime = 10 ** 18 * y + S * mul2 + mul1
+        _dyfprime = D * mul2
+        if yfprime < _dyfprime:
+            y = unsafe_div(y_prev, 2)
+            continue
+        else:
+            yfprime = unsafe_sub(yfprime, _dyfprime)
+        fprime = yfprime // y
+
+        # y -= f / f_prime;  y = (y * fprime - f) / fprime
+        # y = (yfprime + 10**18 * D - 10**18 * S) // fprime + mul1 // fprime * (10**18 - K0) // K0
+        y_minus = mul1 // fprime
+        y_plus = (yfprime + 10 ** 18 * D) // fprime + y_minus * 10 ** 18 // K0
+        y_minus += 10 ** 18 * S // fprime
+
+        if y_plus < y_minus:
+            y = unsafe_div(y_prev, 2)
+        else:
+            y = unsafe_sub(y_plus, y_minus)
+
+        if y > y_prev:
+            diff = unsafe_sub(y, y_prev)
+        else:
+            diff = unsafe_sub(y_prev, y)
+        if diff < max(convergence_limit, unsafe_div(y, 10 ** 14)):
+            frac = unsafe_div(y * 10 ** 18, D)
+            assert (frac > 10 ** 16 - 1) and (frac < 10 ** 20 + 1)  # dev: unsafe value for y
+            return y
+
+    raise Exception("Did not converge")
 
 
 def newton_y(A, gamma, x, D, i):
@@ -117,12 +198,87 @@ def newton_y(A, gamma, x, D, i):
 
 
 def solve_x(A, gamma, x, D, i):
-    return newton_y(A, gamma, x, D, i)
+    # return newton_y(A, gamma, x, D, i)
+    return newton_y_vyper(A, gamma, x, D, i)
 
 
 def solve_D(A, gamma, x):
     D0 = len(x) * geometric_mean(x)  # <- fuzz to make sure it's ok XXX
     return newton_D(A, gamma, x, D0)
+
+
+def solve_D_vyper(ANN, gamma, x_unsorted):
+    """
+    Finding the invariant using Newton method.
+    ANN is higher by the factor A_MULTIPLIER
+    ANN is already A * N**N
+
+    Currently uses 60k gas
+    """
+
+    # Initial value of invariant D is that for constant-product invariant
+    x = x_unsorted
+    if x[0] < x[1]:
+        x = [x_unsorted[1], x_unsorted[0]]
+
+    assert x[0] > 10**9 - 1 and x[0] < 10**15 * 10**18 + 1  # dev: unsafe values x[0]
+    assert x[1] * 10**18 / x[0] > 10**14-1  # dev: unsafe values x[i] (input)
+
+    D = N_COINS * geometric_mean(x)
+    S = x[0] + x[1]
+    __g1k0 = gamma + 10**18
+
+    for i in range(255):
+        D_prev = D
+        assert D > 0
+        # Unsafe division by D is now safe
+
+        # K0 = 10**18
+        # for _x in x:
+        #     K0 = K0 * _x * N_COINS / D
+        # collapsed for 2 coins
+        K0 = unsafe_div(unsafe_div((10**18 * N_COINS**2) * x[0], D) * x[1], D)
+
+        _g1k0 = __g1k0
+        if _g1k0 > K0:
+            _g1k0 = unsafe_sub(_g1k0, K0) + 1  # > 0
+        else:
+            _g1k0 = unsafe_sub(K0, _g1k0) + 1  # > 0
+
+        # D / (A * N**N) * _g1k0**2 / gamma**2
+        mul1 = unsafe_div(unsafe_div(unsafe_div(10**18 * D, gamma) * _g1k0, gamma) * _g1k0 * A_MULTIPLIER, ANN)
+
+        # 2*N*K0 / _g1k0
+        mul2 = unsafe_div(((2 * 10**18) * N_COINS) * K0, _g1k0)
+
+        neg_fprime = (S + unsafe_div(S * mul2, 10**18)) + mul1 * N_COINS // K0 - unsafe_div(mul2 * D, 10**18)
+
+        # D -= f / fprime
+        D_plus = D * (neg_fprime + S) // neg_fprime
+        D_minus = D*D // neg_fprime
+        if 10**18 > K0:
+            D_minus += unsafe_div(D * (mul1 // neg_fprime), 10**18) * unsafe_sub(10**18, K0) // K0
+        else:
+            D_minus -= unsafe_div(D * (mul1 // neg_fprime), 10**18) * unsafe_sub(K0, 10**18) // K0
+
+        if D_plus > D_minus:
+            D = unsafe_sub(D_plus, D_minus)
+        else:
+            D = unsafe_div(unsafe_sub(D_minus, D_plus), 2)
+
+        if D > D_prev:
+            diff = unsafe_sub(D, D_prev)
+        else:
+            diff = unsafe_sub(D_prev, D)
+
+        if diff * 10**14 < max(10**16, D):  # Could reduce precision for gas efficiency here
+            # Test that we are safe with the next newton_y
+            for _x in x:
+                frac = _x * 10**18 // D
+                assert (frac > 10**16 - 1) and (frac < 10**20 + 1)  # dev: unsafe values x[i]
+            return D
+
+    raise Exception("Did not converge")
 
 
 class Curve:
@@ -144,17 +300,62 @@ class Curve:
     def xp(self):
         return [x * p // 10 ** 18 for x, p in zip(self.x, self.p)]
 
-    def D(self):
-        xp = self.xp()
+    def D(self, xp=None):
+        xp = xp or self.xp()
         if any(x <= 0 for x in xp):
             raise ValueError
-        return solve_D(self.A, self.gamma, xp)
+        # return solve_D(self.A, self.gamma, xp)
+        return solve_D_vyper(self.A, self.gamma, xp)
 
-    def y(self, x, i, j):
-        xp = self.xp()
+    def y(self, x, i, j, hack=(None, None)):
+        xp, dy = hack
+        xp = xp or self.xp()
         xp[i] = x * self.p[i] // 10 ** 18
-        yp = solve_x(self.A, self.gamma, xp, self.D(), j)
-        return yp * 10 ** 18 // self.p[j]
+        old_xp = None
+        if dy:
+            old_xp = xp.copy()
+            old_xp[i] = old_xp[i] - dy
+        yp = solve_x(self.A, self.gamma, xp, self.D(xp=old_xp), j)
+        return yp
+
+
+EXP_PRECISION = 1
+
+
+def half_pow(power: int) -> int:
+    """port from Vyper code"""
+
+    intpow = power // 10 ** 18
+    if intpow > 59:
+        return 0
+    otherpow = power - intpow * 10 ** 18  # < 10**18
+    result = 2 ** intpow
+    result = 10 ** 18 // result
+    if otherpow == 0:
+        return result
+
+    term = 10 ** 18
+    S = 10 ** 18
+    neg: bool = False
+
+    for i in range(1, 64):
+        K = i * 10 ** 18  # <= 255 * 10**18; >= 10**18
+        c = K - 10 ** 18  # <= 254 * 10**18; < K
+        if otherpow > c:  # c < otherpow < 10**18 <= K -> c < K
+            c = otherpow - c
+            neg = not neg
+        else:
+            c = c - otherpow  # c < K
+        # c <= 254 * 10**18, < K -> (c/2) / K < 1 -> term * c/2 / K <= 10**18
+        term = (term * (c // 2)) // K
+        if neg:
+            S -= term
+        else:
+            S += term
+        if term < EXP_PRECISION:
+            return result * S // 10 ** 18
+
+    raise "Did not converge"
 
 
 class Trader:
@@ -217,8 +418,8 @@ class Trader:
 
         return geometric_mean(X)
 
-    def update_xcp(self, only_real=False):
-        xcp = self.get_xcp()
+    def update_xcp(self, only_real=False, xcp=None):
+        xcp = xcp or self.get_xcp()
         self.xcp_profit_real = self.xcp_profit_real * xcp // self.xcp
         if not only_real:
             self.xcp_profit = self.xcp_profit * xcp // self.xcp
@@ -252,14 +453,26 @@ class Trader:
         """
         try:
             x_old = self.curve.x[:]
+
             y = self.curve.x[j] + dy
-            x = self.curve.y(y, j, i)
-            dx = self.curve.x[i] - x
-            self.curve.x[i] = x
+
+            # hacking internal pools' representation
+            x = self.curve.x.copy()
+            x[j] += dy
+            hacked_xp = [x * p // 10 ** 18 for x, p in zip(x, self.curve.p)]
+            hacked_xp[j] -= dy * self.curve.p[j] // 10 ** 18
+
+            # calculate swap using internal pools' representation
+            internal_x = self.curve.y(y, j, i, hack=(hacked_xp, dy * self.curve.p[j] // 10 ** 18))
+            internal_dx = hacked_xp[i] - internal_x
+
+            dx = internal_dx * 10 ** 18 // self.curve.p[i]
+            self.curve.x[i] = x_old[i] - dx
             self.curve.x[j] = y
             fee = self.fee()
-            self.curve.x[i] += dx * fee // 10 ** 10
-            dx = dx * (10 ** 10 - fee) // 10 ** 10
+            fee_amount = dx * fee // 10 ** 10
+            self.curve.x[i] += fee_amount
+            dx = dx - fee_amount
             if dx * 10 ** 18 // dy < min_price or dx < 0:
                 self.curve.x = x_old
                 return False
@@ -272,12 +485,14 @@ class Trader:
         # XXX what if every block only has p_b being last
         N = len(price_vector)
         if t > self.t:
-            alpha = 0.5 ** ((t - self.t) / self.ma_half_time)
+            arg = (t - self.t) * int(1e18) // self.ma_half_time
+            alpha = half_pow(arg)
             for k in range(1, N):
-                self.price_oracle[k] = int(price_vector[k] * (1 - alpha) + self.price_oracle[k] * alpha)
+                self.price_oracle[k] = \
+                    (price_vector[k] * (int(1e18) - alpha) + self.price_oracle[k] * alpha) // int(1e18)
             self.t = t
 
-    def tweak_price(self, t, a, b, p):
+    def tweak_price(self, t, a, b, p, debug=False):
         self.ma_recorder(t, self.last_price)
         if b > 0:
             self.last_price[b] = p * self.last_price[a] // 10 ** 18
@@ -302,14 +517,21 @@ class Trader:
         old_profit = self.xcp_profit_real
         old_xcp = self.xcp
 
+        _xp = self.curve.x.copy()
+        _xp[1] = _xp[1] * p_new[1] // old_p[1]
+        _D = solve_D_vyper(self.curve.A, self.curve.gamma, _xp)
+        _xp = [_D // N_COINS, _D * 10 ** 18 // (N_COINS * p_new[1])]
+        self.update_xcp(only_real=True, xcp=geometric_mean(_xp))
         self.curve.p = p_new
-        self.update_xcp(only_real=True)
 
         if 2 * (self.xcp_profit_real - 10 ** 18) <= self.xcp_profit - 10 ** 18:
             # If real profit is less than half of maximum - revert params back
             self.curve.p = old_p
             self.xcp_profit_real = old_profit
             self.xcp = old_xcp
+        else:
+            if debug:
+                print(f"Adjusted price: {old_p[1]} -> {self.curve.p[1]}")
 
         return norm
 
